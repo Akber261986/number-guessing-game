71a8f16986e97dfbf77e51da7ec90bfa5f80ef02
Ghulam Akber <160354667+Akber261986@users.noreply.github.com>
2024-09-28 10:02:26 +0500

    unused variable removed

--- components/number-guessing.tsx
+++ components/number-guessing.tsx
@@ -3,7 +3,6 @@
 import { useState, useEffect, ChangeEvent } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Target } from "lucide-react";
 
 interface NumberGuessingState {
     gameStarted: boolean;
